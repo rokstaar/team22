44559c1b00be5208d1131f5488ed020e8b48c9a1
cjfals <cjfals7292@naver.com>
2023-04-27 09:44:03 +0900

    23.04.26 경매실시간반영

--- Team22/src/main/java/com/market/controller/AuctionController.java
+++ Team22/src/main/java/com/market/controller/AuctionController.java
@@ -2,15 +2,20 @@ package com.market.controller;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -90,14 +95,15 @@ public class AuctionController {
 	@RequestMapping(value = "/bid", method = RequestMethod.GET)
 	public List<Integer> bid(AuctionVO vo, @RequestParam("bidPrice") String price, HttpSession session, @RequestParam("lastBuyer") String lBuyer,
 			@RequestParam("lastBid") String lBid) throws Exception {
+		System.out.println(vo);
+		System.out.println(lBuyer);
+		System.out.println(lBid);
 		List<Integer> List = new ArrayList<Integer>();
 		String id = (String)session.getAttribute("id");
 		int aPrice = Integer.parseInt(price);
 		int lastBid = Integer.parseInt(lBid);
 		vo.setAu_buyerId(id);
 		vo.setAu_bidPrice(aPrice);
-		System.out.println(lBuyer);
-		System.out.println(lastBid);
 		
 		int pPay = lastBid + service.getMpay(lBuyer);
 		
@@ -108,25 +114,25 @@ public class AuctionController {
 		List.add(aPrice);
 		List.add(service.getMpay(id));
 		
-		
 		return List;
-		
 	}
 	
-//	@RequestMapping(value = "/sse")
-//	public SseEmitter handleSse() {
-//		System.out.println("sse@@@@@@@@@@@@");
-//        SseEmitter emitter = new SseEmitter();
-//        nonBlockingService.execute(() -> {
-//            try {
-//                emitter.send("/sse" + " @ " + "dd");
-//                emitter.complete();
-//            } catch (Exception ex) {
-//                emitter.completeWithError(ex);
-//            }
-//        });
-//        return emitter;
-//   }  
+	@ResponseBody
+	@RequestMapping(value = "/uBid", method = RequestMethod.GET)
+	public HashMap<String, Object> uBid(AuctionVO vo, Model model, HttpSession session) throws Exception {
+		String id = (String)session.getAttribute("id");
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		
+		map.put("bid", service.getADetail(vo).getAu_bidPrice());
+		map.put("pay", service.getMpay(id));
+		map.put("buyer", service.getADetail(vo).getAu_buyerId());
+		
+		return map;
+	}
+
+	
+    
+
 
 }
 
